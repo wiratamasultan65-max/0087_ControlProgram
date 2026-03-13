5039c24f603a490151c173d1959d0737932528a8
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-03-13 22:53:55 +0800

    Menambahkan Alur Eksekusi (Flow) FINAL

--- Elself.cpp
+++ Elself.cpp
@@ -14,3 +14,24 @@ string Status_lulus(float r) //Bertugas menerima hasil nilai rata-rata (r) dan m
         return "Gagal";
 }
 
+int main()
+{
+    float nilBI, nilMT, rerata;
+    string status;
+
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilMT;
+
+    // rerata = (nilBI+nilMT)/2;
+    // if (rerata >= 60)
+    //     status = "Lolos";
+    // else
+    //     status = "Gagal";
+
+    rerata = Rata_rata(nilBI, nilMT);
+    // status = Status_lulus(rerata);
+
+    cout << "Status kelulusan = " << Status_lulus(rerata) << ", dengan nilai rerata = " << rerata << endl;
+}
