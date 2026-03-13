5108f81706ee456336e4d8719f14fa1f00bb0e56
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-03-13 22:46:30 +0800

    Final berserta Penjelasan setiap langkahnya

--- Simplef.cpp
+++ Simplef.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
+
+int main()
+{
+    int x;
+
+    srand(time(0)); //Mengatur seed angka acak berdasarkan waktu saat ini
+    x = rand() % 10; // Menghasilkan angka acak bulat antara 0 dan 9
+
+    if (x == 5) //Menambahkan Logika untuk memeriksa apakah x sama dengan 5
+    {           // Maka kita akan mengoreksi nilai x dengan menambahkan 1
+                //Sehingga nilai x tidak lagi 5, tetapi menjadi 6
+        x = x + 1;
+        cout << "nilai x sudah dikoreksi menjadi : " << x << endl;
+    }
+
+    cout << "Bilangan yang diberikan = " << x;
+}
