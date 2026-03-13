3b41a2badd96db3377646926d2212793c5f75d6c
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-03-13 22:56:58 +0800

    FINAL

--- Nestedlf.cpp
+++ Nestedlf.cpp
@@ -1,2 +1,41 @@
 #include <iostream>
 using namespace std;
+
+float Rata_rata(float a, float b)
+{
+    return (a+b)/2;
+}
+
+string Status_lulus(float r)
+{
+    if (r >= 60)
+    {
+        if(r > 80)
+        {
+            return "Lulus Cumlade";
+        }
+        else
+        {
+            return "Lulus Baik";
+        }
+    }
+    else
+    {
+        return "Gagal";
+    }
+}
+
+int main()
+{
+    float nilBI, nilMT, rerata;
+    string status;
+
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilMT;
+
+    rerata = Rata_rata(nilBI, nilMT);
+
+    cout << "Status kelulusan = " << Status_lulus(rerata) << ", dengan nilai rerata = " << rerata << endl;
+}
