c4494765fd1dd6714b5e4cd83806edec07c8c46a
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-03-13 22:52:03 +0800

    Menambahkan Fungsi dengan Parameter

--- Elself.cpp
+++ Elself.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 using namespace std;
+
+float Rata_rata(float a, float b)   //Bertugas menerima dua nilai angka (nilBI dan nilMTK) dan menghitung rata-rata dari kedua nilai tersebut.
+{
+    return (a+b)/2;
+}
+
+string Status_lulus(float r) //Bertugas menerima hasil nilai rata-rata (r) dan menentukan apakah nilai tersebut memenuhi kriteria kelulusan atau tidak.
+{
+    if (r >= 60)
+        return "Lolos";
+    else
+        return "Gagal";
+}
+
